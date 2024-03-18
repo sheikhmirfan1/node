9f5ba4ecd95d2c0a2f8665ccd52a4f49a29de966
Sheikhmirfan1 <Sheikhmirfan@yahoo.com>
2024-03-18 13:42:51 +0100

    created port

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import chalk from "chalk";
-
-
-
-
+import http from "http";
 
 console.log(chalk.blue("Hello Friends"));
 console.log(chalk.red("Paul"));
 console.log(chalk.green("Marla"));
+
+const host = "localhost";
+const port = 8000;
+
+const server = http.createServer((request, response) => {
+ response.end('<h1>Hello World</h1>');
+}); 
+
+server.listen(port, host, () => {
+ console.log(`Server is running on http://${host}:${port}`);
+}
+);
+
+
